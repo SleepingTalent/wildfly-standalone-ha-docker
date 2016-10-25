3e76cc0bf3a5ce0f54244e7487fc41fa31690691
noveria <jaybono30@hotmail.com>
2016-10-25 15:43:19 +0100

    Updated to Work with JGossip Router

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,20 @@ ENV WILDFLY_MANAGEMENT_USER admin
 ENV WILDFLY_MANAGEMENT_PASSWORD admin
 ENV MODCLUSTER_HOST modcluster
 ENV MODCLUSTER_PORT 80
+ENV GOSSIP_ROUTER_HOST 0.0.0.0
+ENV GOSSIP_ROUTER_PORT 12001
 ENV ARTIFACT_NAME cluster-demo.war
 ENV WAIT_TIME_SECS 30
 ENV NETWORK_MODE bridge
 
 ADD ${ARTIFACT_NAME} /opt/jboss/wildfly/standalone/deployments/
-ADD wait-for-it.sh /opt/jboss/wildfly/
+#ADD wait-for-it.sh /opt/jboss/wildfly/
 
 # Add the docker entrypoint script
 ADD entrypoint.sh /opt/jboss/wildfly/bin/entrypoint.sh
-ADD commands.cli /opt/jboss/wildfly/bin/commands.cli
+#ADD commands.cli /opt/jboss/wildfly/bin/commands.cli
+
+ADD standalone-ha-with-gossip.xml /opt/jboss/wildfly/standalone/configuration/
 
 # Change the ownership of added files/dirs to `jboss`
 USER root
@@ -26,7 +30,8 @@ RUN rm -rf /opt/jboss/wildfly/standalone/configuration/standalone_xml_history
 
 RUN chown -R jboss:jboss /opt/jboss/wildfly
 RUN chmod +x /opt/jboss/wildfly/bin/entrypoint.sh
-RUN chmod +x /opt/jboss/wildfly/bin/commands.cli
+#RUN chmod +x /opt/jboss/wildfly/bin/commands.cli
+RUN chmod +x /opt/jboss/wildfly/standalone/configuration/standalone-ha.xml
 USER jboss
 
 EXPOSE 8080 9990 9999 8009 45700 7600 57600
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,13 @@ then
 	echo "=> add-user-process: jboss users have been created"
 fi
 
-sed -i "s/@MODCLUSTER_HOST@/${MODCLUSTER_HOST}/" ${JBOSS_HOME}/bin/commands.cli
-sed -i "s/@MODCLUSTER_PORT@/${MODCLUSTER_PORT}/" ${JBOSS_HOME}/bin/commands.cli
+#sed -i "s/@MODCLUSTER_HOST@/${MODCLUSTER_HOST}/" ${JBOSS_HOME}/bin/commands.cli
+#sed -i "s/@MODCLUSTER_PORT@/${MODCLUSTER_PORT}/" ${JBOSS_HOME}/bin/commands.cli
+
+sed -i "s/@MODCLUSTER_HOST@/${MODCLUSTER_HOST}/" ${JBOSS_HOME}/standalone/configuration/standalone-ha-with-gossip.xml
+sed -i "s/@MODCLUSTER_PORT@/${MODCLUSTER_PORT}/" ${JBOSS_HOME}/standalone/configuration/standalone-ha-with-gossip.xml
+sed -i "s/@GOSSIP_ROUTER_HOST@/${GOSSIP_ROUTER_HOST}/" ${JBOSS_HOME}/standalone/configuration/standalone-ha-with-gossip.xml
+sed -i "s/@GOSSIP_ROUTER_PORT@/${GOSSIP_ROUTER_PORT}/" ${JBOSS_HOME}/standalone/configuration/standalone-ha-with-gossip.xml
 
 
 if [ "${NETWORK_MODE}" = "host" ]
@@ -19,30 +24,28 @@ else
 	IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
 fi
 
-echo "=> configuration-process: starting WildFly server"
-${JBOSS_HOME}/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.bind.address.private=$IPADDR -Djboss.node.name=$HOSTNAME-$IPADDR > /dev/null &
+#echo "=> configuration-process: starting WildFly server"
+#${JBOSS_HOME}/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.bind.address.private=$IPADDR -Djboss.node.name=$HOSTNAME-$IPADDR > /dev/null &
+#echo "=> configuration-process: waiting for WildFly to start on $IPADDR"
 
-echo "=> configuration-process: waiting for WildFly to start on $IPADDR"
 
-
-echo "=> configuration-process: sleeping for ${WAIT_TIME_SECS} seconds"
-sleep ${WAIT_TIME_SECS}
-echo "=> configuration-process: waking up"
+#echo "=> configuration-process: sleeping for ${WAIT_TIME_SECS} seconds"
+#sleep ${WAIT_TIME_SECS}
+#echo "=> configuration-process: waking up"
 	
-echo "=> configuration-process: checking that WildFly standalone has started"
-${JBOSS_HOME}/wait-for-it.sh $IPADDR:9990 -- echo "=> configuration-process: WildFly standalone has started"
+#echo "=> configuration-process: checking that WildFly standalone has started"
+#${JBOSS_HOME}/wait-for-it.sh $IPADDR:9990 -- echo "=> configuration-process: WildFly standalone has started"
 	
-echo "=> configuration-process: executing cli commands"
+#echo "=> configuration-process: executing cli commands"
 #${JBOSS_HOME}/bin/jboss-cli.sh -connect controller=$IPADDR:9999 --user=${WILDFLY_MANAGEMENT_USER} --password=${WILDFLY_MANAGEMENT_PASSWORD} --file=`dirname "$0"`/commands.cli
-${JBOSS_HOME}/bin/jboss-cli.sh -c --controller=$IPADDR:9990 --user=${WILDFLY_MANAGEMENT_USER} --password=${WILDFLY_MANAGEMENT_PASSWORD} --file=${JBOSS_HOME}/bin/commands.cli
+#${JBOSS_HOME}/bin/jboss-cli.sh -c --controller=$IPADDR:9990 --user=${WILDFLY_MANAGEMENT_USER} --password=${WILDFLY_MANAGEMENT_PASSWORD} --file=${JBOSS_HOME}/bin/commands.cli
 #${JBOSS_HOME}/bin/jboss-cli.sh -c --file=`dirname "$0"`/commands.cli
 
-echo "=> configuration-process: shutting down WildFly"
-${JBOSS_HOME}/bin/jboss-cli.sh -c --controller=$IPADDR:9990 --user=${WILDFLY_MANAGEMENT_USER} --password=${WILDFLY_MANAGEMENT_PASSWORD} --command=":shutdown"
-echo "=> configuration-process: configuration complete"
+#echo "=> configuration-process: shutting down WildFly"
+#${JBOSS_HOME}/bin/jboss-cli.sh -c --controller=$IPADDR:9990 --user=${WILDFLY_MANAGEMENT_USER} --password=${WILDFLY_MANAGEMENT_PASSWORD} --command=":shutdown"
+#echo "=> configuration-process: configuration complete"
 
 echo "=> starting Standalone WildFly with HOSTNAME:IPAddress : $HOSTNAME:$IPADDR"
 
-exec ${JBOSS_HOME}/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.bind.address.private=$IPADDR -Djboss.node.name=$HOSTNAME-$IPADDR
+exec ${JBOSS_HOME}/bin/standalone.sh -c standalone-ha-with-gossip.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.bind.address.private=$IPADDR -Djboss.node.name=$HOSTNAME-$IPADDR
 
-#exec ${JBOSS_HOME}/bin/standalone.sh -b $HOSTNAME -bmanagement $HOSTNAME -c standalone-ha.xml
